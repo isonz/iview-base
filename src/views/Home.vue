6dca0c641cb266dff1bc8dd082a6138b7f059742
ison <isonz@qq.com>
2019-07-31 19:08:17 +0800

    asdf

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    {{ data.message }}
+    <IviewButtonHome></IviewButtonHome>
+    <iview-button-home></iview-button-home>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import IviewButtonHome from '@/components/IviewButton'
+
+export default {
+  name: 'home',
+  components: {
+    IviewButtonHome
+  },
+  data() {
+    return {
+      data: {
+        message: '你好',
+      }
+    }
+  }
+}
+
+</script>
